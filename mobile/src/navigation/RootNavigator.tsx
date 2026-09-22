32ac26c0849acab24d3d96a92806c5a59ec6e418
Matias Kruk <kruk.matias@gmail.com>
2026-09-22 23:46:19 +0200

    style(p6): bound authed navigator size

--- mobile/src/navigation/RootNavigator.tsx
+++ mobile/src/navigation/RootNavigator.tsx
@@ -360,9 +360,7 @@ function AuthedNavigator({
       <AuthedStack.Screen name="Home">
         {({ navigation }) => <HomeRoute navigation={navigation} gatewayBaseUrl={gatewayBaseUrl} dubbridgeEnv={dubbridgeEnv} />}
       </AuthedStack.Screen>
-      <AuthedStack.Screen name="MyContent">
-        {({ navigation }) => <MyContentRoute navigation={navigation} gatewayBaseUrl={gatewayBaseUrl} />}
-      </AuthedStack.Screen>
+      <AuthedStack.Screen name="MyContent">{({ navigation }) => <MyContentRoute navigation={navigation} gatewayBaseUrl={gatewayBaseUrl} />}</AuthedStack.Screen>
       <AuthedStack.Screen name="AssetList">
         {({ navigation }) => <AssetListRoute navigation={navigation} gatewayBaseUrl={gatewayBaseUrl} />}
       </AuthedStack.Screen>
